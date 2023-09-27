b656062e26c2f07a280c9433b827b9d0df468eec
root <root@194-67-105-170.cloudvps.regruhosting.ru>
2023-09-27 10:26:50 +0300

    docker_hub1

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,7 +10,7 @@ class TaskiAPITestCase(TestCase):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """Проверка доступности списка задачq."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
